d5755488f3cac5f471bd72b51d55ed09d8d91a07
Di-Danylo <73402941+Di-Danylo@users.noreply.github.com>
2020-12-05 20:11:00 +0200

    Added allowed hosts

--- lab3/my_site/settings.py
+++ lab3/my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ir@y(0y^b0lmf-sa5o0p05+v=^3_+*v&$_hkvph@nezzddd1)@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
