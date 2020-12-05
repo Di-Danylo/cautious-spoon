0708700bc9ddab127d452e25d3ee98ccd5e85020
Di-Danylo <73402941+Di-Danylo@users.noreply.github.com>
2020-12-05 20:41:54 +0200

    Update views.py

--- lab3/main/views.py
+++ lab3/main/views.py
@@ -10,5 +10,5 @@ def main(request):
 
 
 def health(request):
-    response = dict(date= time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), current_page=request.path, server_info=os.uname()[0], client_info=os.getlogin())
+    response = dict(date= time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), current_page=request.path, server_info=os.name, client_info=os.uname()[1])
     return JsonResponse(response)
